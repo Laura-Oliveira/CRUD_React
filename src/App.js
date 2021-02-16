6caf74a4d7f13949c67098ec1a0b582c591d1159
Laura-Oliveira <laura.regina.cp@gmail.com>
2021-02-16 14:56:48 -0300

    Layout da parte de cadastro

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
+function App() 
+{
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="App">
+      <FormRegisterUser />
     </div>
   );
 }
 
+function FormRegisterUser() 
+{
+  return (
+    <form className="form_register_user">
+      <div className="div_form_register_user">
+
+        <legend id="form_register_user_legend"> Cadastrar Usuário </legend>
+
+        {/* Campo Username
+            Input for Username
+        */}
+        <label for="form_register_user_username">
+          <input
+            id="form_register_user_username"
+            type="text"
+            maxLength="100"
+            className="input"
+            placeholder="Nome"
+            required="required"
+          />
+        </label>
+        {/* End */}
+
+        {/* Campo Email Usuário
+            Input for User Email
+        */}
+        <label for="form_register_user_email" className="label">
+          <input
+            id="form_register_user_email"
+            type="email"
+            placeholder="Ex: maria@gmail.com"
+            className="input"
+            required="required"
+          />
+        </label>
+        {/* End */}
+
+        <label for="form_register_user_password" className="label">
+          <input
+            id="form_register_user_password"
+            type="password"
+            placeholder="Senha"
+            className="input"
+            required="required"
+          />
+        </label>
+        {/* End */}
+        <p />
+
+        {/* Botões de Cadastro e Cancelamento de Cadastro
+            Register Button and Cancel Button
+        */}
+        <div id="btn_form_register_user">
+          <label for="btn_form_register_user_submit">
+            <input
+              id="btn_form_register_user_submit"
+              type="button"
+              value="Cadastar"
+              required="required"
+            />
+          </label>
+        </div>
+        {/* End */}
+      </div>
+    </form>
+  );
+}
 export default App;
